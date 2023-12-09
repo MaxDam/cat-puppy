5ab622369bc59100e82bf764f4b2f50bcf78a073
D'Amico Massimiliano <Massimiliano.DAmico@exprivia.com>
2023-12-09 16:35:51 +0100

    get llm

--- puppy.py
+++ puppy.py
@@ -1,6 +1,7 @@
 from langchain.llms import Ollama
 from cat.log import log
 from cat.looking_glass.callbacks import NewTokenHandler
+import copy
 import time
 
 class Puppy:
@@ -9,13 +10,15 @@ class Puppy:
         self.cat = cat
         self.last_response_time = 0
 
-        # Get settings
-        settings = cat.mad_hatter.get_plugin().load_settings()
+        # Get all settings
+        self.settings = cat.mad_hatter.get_plugin().load_settings()
         
-        if "streaming" in settings:
-            del settings["streaming"]
+        # Get only setting for llm puppy
+        llm_settings = copy.copy(self.settings)
+        if "streaming" in llm_settings:
+            del llm_settings["streaming"]
 
-        self.puppy_llm = Ollama(**settings)
+        self.puppy_llm = Ollama(**llm_settings)
 
 
     def llm(self, prompt: str, stream: bool = False) -> str:
