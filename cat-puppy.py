cf357dfaf15906db914e22d60a31ba0601824875
D'Amico Massimiliano <Massimiliano.DAmico@exprivia.com>
2023-12-09 13:39:30 +0100

    class puppy

--- cat-puppy.py
+++ cat-puppy.py
@@ -1,10 +1,8 @@
 from cat.mad_hatter.decorators import tool, hook, plugin
 from pydantic import BaseModel, Field
-from datetime import datetime, date
-from typing import Dict, Optional
+from typing import Dict
 from cat.log import log
-from langchain.llms import Ollama
-import time
+from .puppy import Puppy
 
 
 class MySettings(BaseModel):
@@ -14,7 +12,7 @@ class MySettings(BaseModel):
     )
     model: str = Field(
         title="model",
-        default="openchat"
+        default="zephyr:7b-beta"  #openchat
     )
     num_ctx: int = Field(
         title="num ctx",
@@ -30,7 +28,7 @@ class MySettings(BaseModel):
     )
     temperature: float = Field(
         title="temperature",
-        default=0.8
+        default=0.1
     )
 
 
@@ -57,51 +55,35 @@ def after_cat_bootstrap(cat):
     del cat.working_memory["puppy_llm"]
 
 
-# load puppy llm
-def load_puppy_llm(cat):
-    
-    # Get settings
-    settings = cat.mad_hatter.get_plugin().load_settings()
-    
-    # initialize puppy llm and save it into working memory
-    log.warning(f"Configuring putty llm.. ")
-    try:
-        puppy_llm = Ollama(**settings)
-        cat.working_memory["puppy_llm"] = puppy_llm
-        log.warning(f"The model had been configured: {settings}")
-    except Exception  as e:
-        log.error(f"There was an error in puppy llm initialization: {e}")
-
-
 @hook()
 def agent_fast_reply(fast_reply, cat) -> Dict:
     
-    return_direct = False
-
-    # load puppy llm only fist time
-    if "puppy_llm" not in cat.working_memory.keys():
-        load_puppy_llm(cat)
-
-    # If the cat doesn't call any tools ..
+    # If the cat doesn't call any tools, call puppy
     num_procedural_mems = len( cat.working_memory["procedural_memories"] )
     log.warning(f"Number procedural memories: {num_procedural_mems}")
     if num_procedural_mems == 0:
 
-        # Get the puppy llm from working memory
-        puppy_llm = cat.working_memory["puppy_llm"]
+        # Get puppy
+        puppy = get_puppy(cat)
 
         # Get user message
         user_message = cat.working_memory["user_message_json"]["text"]
 
         # Call puppy llm
-        start_time = time.time()
-        log.warning(f"Calling puppy-llm: {user_message}")
-        response = puppy_llm(user_message)
-        log.warning(f"received response in: {(time.time()-start_time)}")
-        return_direct = True
+        log.warning(f"Calling puppy llm: {user_message}")
+        response = puppy.llm(user_message, stream=True)
+        log.warning(f"received response in: {puppy.last_response_time}")
+        return { "output": response } 
     
-
-    if return_direct:
-        fast_reply = { "output": response }
-        
     return fast_reply
+
+
+# Return puppy llm
+def get_puppy(cat):
+    # load puppy llm only fist time
+    if "puppy_llm" not in cat.working_memory.keys():
+        log.warning(f"Load putty llm.. ")
+        puppy = Puppy(cat)
+        cat.working_memory["puppy_llm"] = puppy
+
+    return cat.working_memory["puppy_llm"]
